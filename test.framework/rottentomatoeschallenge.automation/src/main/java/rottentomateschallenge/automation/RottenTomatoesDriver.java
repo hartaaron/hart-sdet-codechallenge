a0b7d106cb5c79317f83ebcf35a6ea6da77890d2
Aaron Evans <fijiaaron@one-shore.com>
2015-12-01 18:21:11 -0600

    working on using parameterized to get test case values

--- test.framework/rottentomatoeschallenge.automation/src/main/java/rottentomateschallenge/automation/RottenTomatoesDriver.java
+++ test.framework/rottentomatoeschallenge.automation/src/main/java/rottentomateschallenge/automation/RottenTomatoesDriver.java
@@ -1,4 +1,17 @@
 package rottentomateschallenge.automation;
 
+import org.openqa.selenium.WebDriver;
+
 public class RottenTomatoesDriver {
+    WebDriver driver;
+
+    public RottenTomatoesDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getTitle() {
+        return driver.getTitle();
+    }
+
+    public void start() {}
 }
--- test.framework/rottentomatoeschallenge.tests/src/test/java/RottenTomatoesChallenge_Tests.java
+++ test.framework/rottentomatoeschallenge.tests/src/test/java/RottenTomatoesChallenge_Tests.java
@@ -1,20 +1,56 @@
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import rottentomateschallenge.automation.RottenTomatoesDriver;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
 public class RottenTomatoesChallenge_Tests {
 
     RottenTomatoesDriver app;
 
-    @Before
-    public void setUp() {
+    HashMap<String, String> expected;
+
+    static {
+        System.setProperty("webdriver.chrome.driver=/home/aaron/")
+    }
+    public RottenTomatoesChallenge_Tests(HashMap<String, String> expectedResults) {
+        this.expected = expectedResults;
     }
 
+    @Before
+    public void setUp() {
+        // TODO: get webdriver instance from a factory based on config settings
+        WebDriver driver = new ChromeDriver();
+
+        app = new RottenTomatoesDriver(driver);
+    }
 
     @Test
     public void it_should_display_the_app_title() {
-
+        app.start();
+        assertThat(app.getTitle()).isEqualTo(expected.get("title"));
     }
+
+
+    //TODO: get expected results from external data source
+    @Parameters
+    public static Iterable data() {
+        HashMap<String, String> expectedValues = new HashMap<>();
+        expectedValues.put("title", "Rotten Tomatoes Challenge");
+
+        ArrayList<HashMap<String, String>> expectedResults = new ArrayList<>();
+        expectedResults.add(expectedValues);
+
+        return expectedResults;
+    }
+
 }
