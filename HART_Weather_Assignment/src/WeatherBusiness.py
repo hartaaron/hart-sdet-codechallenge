e5f7e1feff0971ca2bf8878f78469d8d044d236f
Dan Development <danny@nextdeavor.com>
2016-08-11 13:28:46 -0700

    added data driven and instructions on readme

--- HART_Weather_Assignment/src/WeatherBusiness.py
+++ HART_Weather_Assignment/src/WeatherBusiness.py
@@ -1,8 +1,11 @@
 import requests
 
 class ParkDecider:
-    def __init__(self, mock=False, parks=["CA,Anaheim", "Orlando,Florida"], mock_winner='CA,Anaheim'):
+    def __init__(self, idea_temp=float(78),mock=False, parks=["CA,Anaheim", "FL,Orlando"], mock_winner='CA,Anaheim'):
         self.parks = parks
+        self.idea_temp = idea_temp
+
+        #TODO decouple using
         self.mock = mock
         self.base_url = 'http://api.wunderground.com/api/06c8fec6a3511479/conditions/q/'
         self.mock_winner = mock_winner
@@ -11,7 +14,16 @@ class ParkDecider:
         if self.mock:
             return self.mock_winner
         else:
-            pass
+            winning_park = None
+            winning_diff = None
+            for park in self.parks:
+                temperature = self.getWeatherTemp(park)  #TODO assume no repeat
+                temp_diff = abs(self.idea_temp - temperature)
+                if winning_diff is None or temp_diff < winning_diff:
+                    winning_park = park
+                    winning_diff = temp_diff
+
+            return winning_park
 
     def getWeatherTemp(self, park):
         temperature = None
@@ -27,5 +39,3 @@ class ParkDecider:
                 return "park defined or rest call threw an exception"
 
         return float(temperature)
-
-    
